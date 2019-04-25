b1b0532795dd406238e9515fed48a159c9cdef71
KamilaZoldyek <kmilawicked@hotmail.com>
2019-04-25 13:56:23 -0300

    main layout

--- app/src/main/java/com/example/kamilazoldyek/theguardianreader/activity/MainActivity.java
+++ app/src/main/java/com/example/kamilazoldyek/theguardianreader/activity/MainActivity.java
@@ -12,57 +12,67 @@ import com.example.kamilazoldyek.theguardianreader.model.News;
 import com.example.kamilazoldyek.theguardianreader.model.Response;
 import com.example.kamilazoldyek.theguardianreader.model.Result;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 
+import static com.example.kamilazoldyek.theguardianreader.constants.Constants.API_KEY;
 import static com.example.kamilazoldyek.theguardianreader.constants.Constants.MAIN_QUERY;
 import static com.example.kamilazoldyek.theguardianreader.constants.Constants.TEST_TAG;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView textViewResult;
-    private Api api;
-    public ApiClient apiClient;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_teste);
-        textViewResult = findViewById(R.id.text_view_result);
-        apiClient.getApi();
-        getNews();
+        setContentView(R.layout.main_activity);
+       // textViewResult = findViewById(R.id.text_view_result);
+        //getNews("1");
 
     }
 
-    private void getNews(){
-        Call<Response> call = apiClient.getInstance().getApi().getNews(MAIN_QUERY);
-        call.enqueue(new Callback<Response>() {
+    private void getNews(String page_number){
+
+        Call<News> call2 = ApiClient.getInstance().getApi().getNews(page_number, API_KEY);
+        call2.enqueue(new Callback<News>() {
+            List<Result> list_news = new ArrayList<>();
             @Override
-            public void onResponse(Call<Response> call, retrofit2.Response<Response> response) {
-                Log.i(TEST_TAG,response.message() );
+            public void onResponse(Call<News> call, retrofit2.Response<News> response) {
                 if(!response.isSuccessful()){
                     textViewResult.setText("Code: "+ response.code());
+                    if (response.code()==401) {
+                        textViewResult.setText("Code 401: Unauthorized \n\n" );
+                    }
+
                     return;
                 }
-                List<Result> list_news = response.body().results;
+                list_news = response.body().getResponse().getResults();
 
                 for (Result news : list_news){
                     String content = "";
                     content += "Title: " + news.getWebTitle() + "\n";
                     content += "Section: " + news.getSectionName() + "\n";
-                    content += "Date: " + news.getWebPublicationDate() + "\n";
+                    content += "Date: " + news.getWebPublicationDate() + "\n\n";
                     textViewResult.append(content);
                 }
             }
 
             @Override
-            public void onFailure(Call<Response> call, Throwable t) {
+            public void onFailure(Call<News> call, Throwable t) {
                 textViewResult.setText(t.getMessage());
+
             }
         });
+
+
 
     }
 
--- app/src/main/java/com/example/kamilazoldyek/theguardianreader/api/Api.java
+++ app/src/main/java/com/example/kamilazoldyek/theguardianreader/api/Api.java
@@ -4,16 +4,24 @@ import com.example.kamilazoldyek.theguardianreader.model.News;
 import com.example.kamilazoldyek.theguardianreader.model.Response;
 
 import java.util.List;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+import retrofit2.http.QueryMap;
 import retrofit2.http.Url;
 
+import static com.example.kamilazoldyek.theguardianreader.constants.Constants.API_KEY;
 import static com.example.kamilazoldyek.theguardianreader.constants.Constants.MAIN_QUERY;
 
 public interface Api {
 
-    @GET
-    Call<Response> getNews(@Url String url);
+    @GET("search")
+    Call<News> getNews(
+            @Query("page") String page,
+            @Query("api-key") String apiKey
+    );
 }
